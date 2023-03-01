8eca5c332ce7e6c85b11593e8488366bcde8b030
Ryan Arora <arora.ryan@gmail.com>
2023-03-01 01:50:33 -0500

    project boilerplate with prettier and typescript

---
+++ prettier.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-organize-imports"),
+    require.resolve("prettier-plugin-packagejson"),
+  ],
+};
