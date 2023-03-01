fdd810b06f4c035c2915f7116b3aa5030f0b3936
Ryan Arora <arora.ryan@gmail.com>
2023-03-01 02:55:47 -0500

    format

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [
-    //  require.resolve("prettier-plugin-organize-imports"),
+    require.resolve("prettier-plugin-organize-imports"),
     require.resolve("prettier-plugin-packagejson"),
   ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import express from "express";
+import React from "react";
 import { renderToString } from "react-dom/server";
+import App from "./client/App";
 
 const server = express();
-import App from "./client/App";
 
 server.get("*", (req, res) => {
   const html = renderToString(<App />);
